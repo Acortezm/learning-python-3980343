7002768f05963ff143114b4671836d912a341f77
Anabel Cortez Muñoz <90518924+Acortezm@users.noreply.github.com>
2025-10-12 16:15:34 +0000

    read file

--- Start/Ch6/readfile_start.py
+++ Start/Ch6/readfile_start.py
@@ -5,5 +5,12 @@
 
     
 # Open the file and read the contents
-
+sample_file = open("textfile.txt", "r")
+contents = ""
+if sample_file.mode == 'r':
+    contents = sample_file.read()
+    print(contents)
     # use the read() function to read the entire file
+    file_lines = sample_file.readlines()
+    for line in file_lines:
+        print(line)
