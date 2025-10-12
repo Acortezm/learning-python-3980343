883056804a60341bed03273c2f122a8e8f805f5d
Anabel Cortez Muñoz <90518924+Acortezm@users.noreply.github.com>
2025-10-12 14:06:55 +0000

    Classes

--- Start/Ch3/classes_start.py
+++ Start/Ch3/classes_start.py
@@ -2,3 +2,42 @@
 # Example file for working with classes
 #
 
+class Vehicle: 
+  def __init__(self, bodystyle):
+    self.bodystyle = bodystyle
+  
+  def drive(self, speed):
+    self.mode = "driving"
+    self.speed = speed
+
+class Car(Vehicle):
+  def __init__(self, enginetype) :
+    super().__init__("Car")
+    self.wheels = 4
+    self.doors = 4
+    self.engine = enginetype
+  
+  def drive(self, speed):
+    super().drive(speed)
+    print("I drive my" + self.engine + " car at" + str(self.speed) + "mph")
+
+class Motorcycle(Vehicle):
+  def __init__(self, enginetype, hassidecar):
+    super().__init__("Motorcycle")
+    if (hassidecar): 
+      self.wheels = 3
+    else: 
+      self.wheels = 2
+
+    self.foors = 0 
+    self.engine = enginetype
+
+  def drive(self, speed):
+    super().drive(speed)
+    print("I drive my" + self.engine + " car at" + str(self.speed) + "mph")
+
+car1 = Car("gas")
+car2 = Car("electric")
+mc1 = Motorcycle("gas", True)
+print(car1.drive(30))
+print(mc1.drive(10))
